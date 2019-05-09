4a2f0fc79bbf88a6a259bf728250339d115f5db9
tanyadixit21 <tanya2911dixit@gmail.com>
2019-05-09 13:05:10 +0530

    NBOA: A simple question answer service.
    A basic question answer service built using Django, Bootstrap, crispy
    forms and Django templates.

---
+++ nobullshitonlyanswers/qa/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from qa.models import Question
+from qa.models import Answer
+
+class QuestionForm(forms.ModelForm):
+    user = forms.ModelChoiceField(
+                    widget = forms.HiddenInput,
+                    queryset = get_user_model().objects.all(),
+                    disabled = True,)
+
+    class Meta:
+        model = Question
+        fields = ['title', 'question', 'user']
+
+class AnswerForm(forms.ModelForm):
+    user = forms.ModelChoiceField(
+                    widget =  forms.HiddenInput,
+                    queryset = get_user_model().objects.all(),
+                    disabled = True,)
+
+    question = forms.ModelChoiceField(
+                    widget = forms.HiddenInput,
+                    queryset = Question.objects.all(),
+                    disabled = True,)
+
+    class Meta:
+        model = Answer
+        fields = ['answer', 'question', 'user']
+
+class AnswerAcceptanceForm(forms.ModelForm):
+    accepted = forms.BooleanField(widget = forms.HiddenInput,
+                                    required = False,
+                                    )
+
+    class Meta:
+        model = Answer
+        fields = ['accepted',]
